84ab9878750ce5cd4fe960515e10543ef49108e7
emmas96 <45456353+emmas96@users.noreply.github.com>
2019-07-10 08:39:12 +0200

    Update path_config.py

--- util/path_config.py
+++ util/path_config.py
@@ -4,7 +4,8 @@ open3d_path = 'Open3D/build/lib/'
 tc_path = 'SummerInternship/'
 
 sys.path.append(open3d_path)
-from py3d import *
+#from py3d import *
+from open3d import *
 
 def get_tc_path():
 	return tc_path
