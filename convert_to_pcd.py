a8c9902bed9bf85556e74f59641a10ef6b0dd190
emmas96 <45456353+emmas96@users.noreply.github.com>
2019-07-11 13:45:13 +0200

    Update convert_to_pcd.py

--- convert_to_pcd.py
+++ convert_to_pcd.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(description='Choose scan')
 parser.add_argument('key', type=str, metavar='N', help='key to scan, example: sg27_station1')
 args = parser.parse_args()
 
-path = os.path.join(inputFiles/, args.key, scan.txt)
+path = os.path.join('inputFiles/', args.key, 'scan.txt')
 
 data = pd.read_csv(path, sep=" ", header=None)
 data.columns = ["x", "y", "z", "intensity", "r", "g", "b"]
@@ -17,5 +17,5 @@ data.to_csv(r'tmp.xyzrgb', header=False, index=False, sep=" ", mode="a")
 pcd = o3d.io.read_point_cloud('tmp.xyzrgb')
 #os.system("rm tmp.xyzrgb")
 
-path = os.path.join(inputFiles/, args.key, scan.pcd)
+path = os.path.join('inputFiles/', args.key, 'scan.pcd')
 o3d.io.write_point_cloud(path, pcd)
