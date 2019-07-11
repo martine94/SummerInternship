de92e5880b20532e25939333282e6b5f2ed717a8
emmas96 <45456353+emmas96@users.noreply.github.com>
2019-07-11 14:22:48 +0200

    Update convert_to_pcd.py

--- convert_to_pcd.py
+++ convert_to_pcd.py
@@ -9,13 +9,18 @@ args = parser.parse_args()
 
 path = os.path.join('inputFiles/', args.key, 'scan.txt')
 
+print('Reading scan.txt file')
 data = pd.read_csv(path, sep=" ", header=None)
 data.columns = ["x", "y", "z", "intensity", "r", "g", "b"]
+print('Dropping intensity column')
 data = data.drop(columns="intensity")
 
+print('Writing to tmp.xyzrgb')
 data.to_csv(r'tmp.xyzrgb', header=False, index=False, sep=" ", mode="a")
+print('Read tmp.xyzrgb to pcd')
 pcd = o3d.io.read_point_cloud('tmp.xyzrgb')
-#os.system("rm tmp.xyzrgb")
+os.system("rm tmp.xyzrgb")
 
+print('Write to scan.pcd')
 path = os.path.join('inputFiles/', args.key, 'scan.pcd')
 o3d.io.write_point_cloud(path, pcd)
