6d30a1953ee14459f586c78245377d9f7fe388c1
emmas96 <45456353+emmas96@users.noreply.github.com>
2019-07-12 11:25:06 +0200

    revert number of cores to original

--- util/cloud.py
+++ util/cloud.py
@@ -307,8 +307,7 @@ def get_scan_part_out(par, point=None, sample_type='POINT'):
 def get_batch_array(scan_var, par, points=None):
 	global scan
 	scan = scan_var
-	#num_cores = multiprocessing.cpu_count()  
-	num_cores = 1 # ES
+	num_cores = multiprocessing.cpu_count()
 
 	if points is None:
 		pts = []
