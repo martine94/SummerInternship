74859b064c8e4ee4b8a6f9aef6ee610d92929508
martine94 <38217717+martine94@users.noreply.github.com>
2019-08-12 12:15:27 +0200

    Update precompute.py

--- util/precompute.py
+++ util/precompute.py
@@ -27,6 +27,8 @@ def run_precompute(config):
 		for rot in range(0, p.num_rotations):
 			cloud_file = os.path.join(p.dataset_dir, scan_name, "scan.pcd")
 			label_file = os.path.join(p.dataset_dir, scan_name, "scan.labels")
+			print(cloud_file)
+			print(label_file)
 			
 			if not os.path.exists(cloud_file) or not os.path.exists(label_file):
 				print("continue")
