98ba85ce75e02562f493144ea41767282abdec25
martine94 <38217717+martine94@users.noreply.github.com>
2019-08-19 10:53:16 +0200

    Update dataset_params.py

--- util/dataset_params.py
+++ util/dataset_params.py
@@ -69,7 +69,7 @@ class semantic3d_params:
 		
 class forkdata_params:
 	def __init__(self):
-		self.class_freq = np.asarray([0.782, 6.845])
+		self.class_freq = np.asarray([6.845])
 		self.class_weights = -np.log(self.class_freq / 100.0)
 		self.num_classes = len(self.class_freq) + 1
 		self.color_map = [[0, 130, 200], # unlabeled (white)
