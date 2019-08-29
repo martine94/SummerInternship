981822ec06bf04a9aa129867da6b197b5c1478aa
martine94 <38217717+martine94@users.noreply.github.com>
2019-08-29 17:45:21 +0200

    Update path_config.py

--- util/path_config.py
+++ util/path_config.py
@@ -1,7 +1,7 @@
 import sys
 
-open3d_path = '/media/local-data/Student/Open3D/build/lib/'
-tc_path = '/media/local-data/Student/tangent_conv/SummerInternship/'
+open3d_path = '/home/student/Open3D/build/lib/'
+tc_path = '/home/student/SummerInternship/'
 
 sys.path.append(open3d_path)
 from py3d import *
